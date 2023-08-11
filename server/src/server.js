4c2a3125b95d68292624828bf70fbfc7e58f6da8
Jon Mico <jonmico@gmail.com>
2023-08-10 23:51:58 -0400

    Set up database connection.

--- server/src/server.js
+++ server/src/server.js
@@ -1,9 +1,22 @@
 import express from 'express';
+import mongoose from 'mongoose';
 import 'dotenv/config';
 
 const app = express();
 
 const PORT = process.env.PORT ?? 3000;
+const CONNECTION = process.env.MONGOOSE_CONNECTION_STRING;
+
+async function connectDB() {
+  try {
+    await mongoose.connect(CONNECTION);
+    console.log('Connected to database.');
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+connectDB();
 
 app.listen(PORT, () => {
   console.log(`app is listening on port ${3000}`);
