18d5087117490f3f96b04dcb536e8db82fbe4f11
Jon Mico <jonmico@gmail.com>
2023-08-15 21:12:39 -0400

    Add routes for getting specific event and editing a specific event.

--- server/src/controllers/events.js
+++ server/src/controllers/events.js
@@ -9,11 +9,39 @@ export async function getEvents(req, res, next) {
   }
 }
 
+export async function getEvent(req, res, next) {
+  try {
+    const { id } = req.params;
+    const event = await EventModel.findById(id).exec();
+
+    if (!event) throw new AppError(404, 'Event not found.');
+
+    res.json(event);
+  } catch (err) {
+    next(err);
+  }
+}
+
 export async function createEvent(req, res, next) {
   try {
     const newEvent = new EventModel(req.body);
     await newEvent.save();
     res.status(201).json(newEvent);
+  } catch (err) {
+    next(err);
+  }
+}
+
+export async function editEvent(req, res, next) {
+  try {
+    const { id } = req.params;
+    const event = await EventModel.findByIdAndUpdate(id, req.body, {
+      new: true,
+    }).exec();
+
+    if (!event) throw new AppError(404, 'Event not found');
+
+    res.json(event);
   } catch (err) {
     next(err);
   }
--- server/src/routes/events.js
+++ server/src/routes/events.js
@@ -2,12 +2,23 @@ import { Router } from 'express';
 import validateEvent from '../middleware/validateEvent.js';
 import EventModel from '../models/event.js';
 
-import { createEvent, getEvents } from '../controllers/events.js';
+import {
+  createEvent,
+  editEvent,
+  getEvent,
+  getEvents,
+} from '../controllers/events.js';
+
+import AppError from '../AppError.js';
 
 const router = Router();
 
 router.get('/', getEvents);
 
+router.get('/:id', getEvent);
+
 router.post('/', validateEvent, createEvent);
+
+router.put('/:id', validateEvent, editEvent);
 
 export default router;
