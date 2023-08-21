b05b56e98ef461a0eb06bd466bdbc1d746335945
Jon Mico <jonmico@gmail.com>
2023-08-20 23:20:51 -0400

    Add some extra touches to Navbar.

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -4,19 +4,21 @@ import styles from './Navbar.module.css';
 
 export default function Navbar() {
   return (
-    <nav className={styles.navbar}>
-      <div>
-        <NavLink className={`${styles.link} ${styles.main}`}>
-          Event Tracker
-        </NavLink>
-      </div>
-      <div>
+    <div className={styles.navbarWrapper}>
+      <nav className={styles.navbar}>
         <div>
-          <NavLink className={styles.link}>Sign Up</NavLink>
-          <NavLink className={styles.link}>Login</NavLink>
-          <NavLink className={styles.link}>Not Sure Yet</NavLink>
+          <NavLink className={`${styles.link} ${styles.main}`}>
+            Event Tracker
+          </NavLink>
         </div>
-      </div>
-    </nav>
+        <div>
+          <div>
+            <NavLink className={`${styles.link}`}>Sign Up</NavLink>
+            <NavLink className={`${styles.link}`}>Login</NavLink>
+            <NavLink className={`${styles.link}`}>Not Sure Yet</NavLink>
+          </div>
+        </div>
+      </nav>
+    </div>
   );
 }
