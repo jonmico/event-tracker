b85a31f5b1bd55408ff90f636a152b3f49586573
Jon Mico <jonmico@gmail.com>
2023-08-23 11:36:08 -0400

    Add error handling for fetchEvents.

---
+++ client/src/components/EventItem/EventItem.jsx
@@ -0,0 +1,9 @@
+import styles from './EventItem.module.css';
+
+export default function EventItem() {
+  return (
+    <div>
+      <h1>This is an EventItem!</h1>
+    </div>
+  );
+}
--- client/src/components/EventList/EventList.jsx
+++ client/src/components/EventList/EventList.jsx
@@ -1,27 +1,42 @@
 import { useState, useEffect } from 'react';
 
+import EventItem from '../EventItem/EventItem';
+
 const BASE_URL = import.meta.env.VITE_APP_BASE_URL;
 
 export default function EventList() {
-  const [eventList, setEventList] = useState();
+  const [eventList, setEventList] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [eventListError, setEventListError] = useState('');
 
   useEffect(() => {
     async function fetchEvents() {
-      const res = await fetch(`${BASE_URL}/api/events`, {
-        method: 'GET',
-        headers: { 'Content-Type': 'application/json' },
-      });
-      const data = await res.json();
-      setEventList(data);
-      console.log(data);
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}/api/events`, {
+          method: 'GET',
+          headers: { 'Content-Type': 'application/json' },
+        });
+
+        if (!res.ok) {
+          throw new Error(`Oops! Looks like there was an error: ${res.status}`);
+        }
+        const data = await res.json();
+        setEventList(data);
+        console.log(data);
+      } catch (err) {
+        setEventListError(err.message);
+      } finally {
+        setIsLoading(false);
+      }
     }
     fetchEvents();
   }, []);
 
   return (
     <div>
-      {eventList?.map((event) => (
-        <p key={event._id}>{event.name}</p>
+      {eventList.map((event) => (
+        <EventItem key={event._id}>{event.name}</EventItem>
       ))}
     </div>
   );
