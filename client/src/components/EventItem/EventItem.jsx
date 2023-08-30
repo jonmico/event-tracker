fb1c253e520782cd2ba74fd7f7fd4998edfd05bc
Jon Mico <jonmico@gmail.com>
2023-08-30 12:24:05 -0400

    Fix some styling for EventList so dropdown menu works better.

--- client/src/components/EventItem/EventItem.jsx
+++ client/src/components/EventItem/EventItem.jsx
@@ -1,20 +1,28 @@
+import { useState } from 'react';
 import styles from './EventItem.module.css';
 
 export default function EventItem({ event }) {
+  const [isDropDown, setIsDropDown] = useState(false);
+
   const isoString = event.date;
   const date = new Date(isoString);
 
   return (
     <li className={styles.listItem}>
-      <h3 className={styles.listItemData}>{event.name}</h3>
-      <p className={styles.listItemData}>{event.location.name}</p>
-      <p className={styles.listItemData}>
-        {date.toLocaleString('en-US', {
-          month: 'numeric',
-          day: 'numeric',
-          year: 'numeric',
-        })}
-      </p>
+      <div className={styles.eventItem}>
+        <h3 className={styles.eventItemData}>{event.name}</h3>
+        <p className={styles.eventItemData}>{event.location.name}</p>
+        <p className={styles.eventItemData}>
+          {date.toLocaleString('en-US', {
+            month: 'numeric',
+            day: 'numeric',
+            year: 'numeric',
+          })}
+        </p>
+      </div>
+      {/* <div>
+        <p>more info here</p>
+      </div> */}
     </li>
   );
 }
