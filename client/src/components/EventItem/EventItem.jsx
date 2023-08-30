b00cec414e972db0ddf845ab07d748e3445dd7da
Jon Mico <jonmico@gmail.com>
2023-08-30 13:08:26 -0400

    Try to add a toggle button for the dropdown.

--- client/src/components/EventItem/EventItem.jsx
+++ client/src/components/EventItem/EventItem.jsx
@@ -2,16 +2,25 @@ import { useState } from 'react';
 import styles from './EventItem.module.css';
 
 export default function EventItem({ event }) {
-  const [isDropDown, setIsDropDown] = useState(false);
+  const [isDroppedDown, setIsDroppedDown] = useState(false);
 
   const isoString = event.date;
   const date = new Date(isoString);
 
+  function handleClick() {
+    setIsDroppedDown((prev) => !prev);
+  }
+
   return (
     <li className={styles.listItem}>
       <div className={styles.eventItem}>
-        <h3 className={styles.eventItemData}>{event.name}</h3>
-        <p className={styles.eventItemData}>{event.location.name}</p>
+        <span onClick={handleClick} className={styles.toggleButton}>
+          {isDroppedDown ? '-' : '+'}
+        </span>
+        <h3 className={`${styles.eventItemData} ${styles.eventName}`}>
+          {event.name}
+        </h3>
+        <p className={styles.eventItemData}>{event.location?.name}</p>
         <p className={styles.eventItemData}>
           {date.toLocaleString('en-US', {
             month: 'numeric',
@@ -20,9 +29,18 @@ export default function EventItem({ event }) {
           })}
         </p>
       </div>
-      {/* <div>
-        <p>more info here</p>
-      </div> */}
+      {isDroppedDown && (
+        <div className={styles.dropDownData}>
+          <div>
+            <h4>Author</h4>
+            <p>{event.author.firstName}</p>
+          </div>
+          <div>
+            <h4>Time</h4>
+            <p>{event.time}</p>
+          </div>
+        </div>
+      )}
     </li>
   );
 }
