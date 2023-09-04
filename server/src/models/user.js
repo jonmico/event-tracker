957015dacf7aeb465fbae0a7b2d31d477a2753da
Jon Mico <jonmico@gmail.com>
2023-09-03 20:00:58 -0400

    Add password stuff to user model, create secretToken util file.

--- server/src/models/user.js
+++ server/src/models/user.js
@@ -1,14 +1,23 @@
 import mongoose, { mongo } from 'mongoose';
+import bcrypt from 'bcrypt';
 const { Schema } = mongoose;
 
-export const userSchema = new Schema({
-  firstName: { type: String, required: true },
-  lastName: { type: String, required: true },
-  email: { type: String, required: true },
-  phone: { type: String, required: true },
-  organization: { type: String },
-  attendingEvents: [{ type: Schema.Types.ObjectId, ref: 'Event' }],
-  waitlistedEvents: [{ type: Schema.Types.ObjectId, ref: 'Event' }],
+export const userSchema = new Schema(
+  {
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    email: { type: String, unique: true, required: true },
+    password: { type: String, required: true },
+    phone: { type: String, required: true },
+    organization: { type: String },
+    attendingEvents: [{ type: Schema.Types.ObjectId, ref: 'Event' }],
+    waitlistedEvents: [{ type: Schema.Types.ObjectId, ref: 'Event' }],
+  },
+  { timestamps: true }
+);
+
+userSchema.pre('save', async function () {
+  this.password = await bcrypt.hash(this.password, 10);
 });
 
 const UserModel = mongoose.model('User', userSchema);
